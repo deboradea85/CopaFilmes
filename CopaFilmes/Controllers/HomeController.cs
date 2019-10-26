f4616f33d945ea0ac67275c226ac7f4307091936
deboradea85 <debora_dea@hotmail.com>
2019-10-26 19:24:25 -0300

    arquivos iniciais do projeto

---
+++ CopaFilmes/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CopaFilmes.Models;
+
+namespace CopaFilmes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ObterResultadoCampeonato(string filmes)
+        {
+            return null;
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Model/FilmeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Model
+{
+   
+    public class FilmeModel
+    {
+        public FilmeModel(string id, string titulo, int anoLancamento, float nota)
+        {
+            this.id = id;
+            this.titulo = titulo;
+            this.anoLancamento = anoLancamento;
+            this.nota = nota;
+        }
+        public string id { get; private set; }
+       public string titulo { get; private set; }
+       public int anoLancamento { get; private set; }
+       public float nota { get; private set; }
+    }
+}
